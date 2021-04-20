efa9e3a1c3c2e9934861c6bda16d012ea0c0df9e
theunmanifested <waltersoso80@gmail.com>
2021-04-20 12:05:50 -0500

    Create Boot/Rest project

---
+++ Mangiamici/src/main/java/com/skilldistillery/mangiamici/MangiamiciApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.mangiamici;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangiamiciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangiamiciApplication.class, args);
+	}
+
+}
