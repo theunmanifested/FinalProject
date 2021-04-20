e24cb29577659f7b410a0c7ebc1a76fd3b71cdb8
Sofia <sgkartist@gmail.com>
2021-04-20 13:48:46 -0600

    Inception of Angular project, @CrossOrigin added.

--- Mangiamici/src/main/java/com/skilldistillery/mangiamici/controllers/UserController.java
+++ Mangiamici/src/main/java/com/skilldistillery/mangiamici/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.skilldistillery.mangiamici.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.mangiamici.services.UserService;
 
+@CrossOrigin({ "*", "http://localhost:4290" })
 @RequestMapping("api")
 @RestController
 public class UserController {
