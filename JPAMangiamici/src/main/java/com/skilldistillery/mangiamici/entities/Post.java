ef09bc9f91570bfcc0574e329fab4bd8ce8da316
Sofia <sgkartist@gmail.com>
2021-04-21 10:29:29 -0600

    Post and User base mappings

--- JPAMangiamici/src/main/java/com/skilldistillery/mangiamici/entities/Post.java
+++ JPAMangiamici/src/main/java/com/skilldistillery/mangiamici/entities/Post.java
@@ -1,5 +1,113 @@
 package com.skilldistillery.mangiamici.entities;
 
-public class Post {
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user;
+	
+//	@Column(name = "created_at")
+//    @CreationTimestamp
+//    private LocalDateTime createdAt;
+	
+    @Column(name = "updated_date")
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
+    
+    @Column(name="event_date")
+    private LocalDateTime eventDate;
+    
+    @Column(name="post_text")
+    private String postText;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDateTime getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDateTime updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	public LocalDateTime getEventDate() {
+		return eventDate;
+	}
+
+	public void setEventDate(LocalDateTime eventDate) {
+		this.eventDate = eventDate;
+	}
+
+	public String getPostText() {
+		return postText;
+	}
+
+	public void setPostText(String postText) {
+		this.postText = postText;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", user=" + user + ", updatedDate=" + updatedDate + ", eventDate=" + eventDate
+				+ ", postText=" + postText + "]";
+	}
+    
+//    @ManyToOne
+//	@JoinColumn(name="restaurant_id")
+//    private Restaurant restaurant;
+    
+    
 }
--- JPAMangiamici/src/main/java/com/skilldistillery/mangiamici/entities/User.java
+++ JPAMangiamici/src/main/java/com/skilldistillery/mangiamici/entities/User.java
@@ -1,5 +1,8 @@
 package com.skilldistillery.mangiamici.entities;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +16,37 @@ public class User {
 	private int id;
 	
 	private String username;
+	
+//	private Location location
+	
+	private String password;
+	
+	private Boolean enabled;
+	
+	private String role;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="created_date")
+	private LocalDateTime createdDate; 
+	
+	@Column(name="updated_date")
+	private LocalDateTime updatedDate; 
+	
+	@Column(name="img_url")
+	private String imgUrl;
+	
+	@Column(name="promo_opt")
+	private Boolean promoOpt;
+	
+	@Column(name="about_me")
+	private String aboutMe;
+	
+	
 
 	public User() {
 		super();
@@ -32,6 +66,80 @@ public class User {
 
 	public void setUsername(String username) {
 		this.username = username;
+	}
+	
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDateTime updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Boolean getPromoOpt() {
+		return promoOpt;
+	}
+
+	public void setPromoOpt(Boolean promoOpt) {
+		this.promoOpt = promoOpt;
+	}
+
+	public String getAboutMe() {
+		return aboutMe;
+	}
+
+	public void setAboutMe(String aboutMe) {
+		this.aboutMe = aboutMe;
 	}
 
 	@Override
