ce549bb4a0abf6a319b03e4e6682e81c88e1bc46
theunmanifested <waltersoso80@gmail.com>
2021-04-24 13:28:44 -0500

    Finish all models with just composite key models left blank if/until needed

--- ngMangiamici/src/app/models/post.ts
+++ ngMangiamici/src/app/models/post.ts
@@ -1,2 +1,24 @@
+import { Restaurant } from "./restaurant";
+import { User } from "./user";
+
 export class Post {
+
+  id: number;
+  user: User;
+  postText: string;
+  flagged: boolean;
+  enabled: boolean;
+  restaurant: Restaurant;
+
+  constructor(id?: number, user?: User, postText?: string, flagged?: boolean,
+              enabled?: boolean, restaurant?: Restaurant) {
+
+  this.id = id;
+  this.user = user;
+  this.postText = postText;
+  this.flagged = flagged;
+  this.enabled = enabled;
+  this.restaurant = restaurant;
+  }
+
 }
---
+++ ngMangiamici/src/app/models/review-comment.spec.ts
@@ -0,0 +1,7 @@
+import { ReviewComment } from './review-comment';
+
+describe('ReviewComment', () => {
+  it('should create an instance', () => {
+    expect(new ReviewComment()).toBeTruthy();
+  });
+});
