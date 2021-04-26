ad4fc564fad37939987251a18197fcc3eb1112f5
Sofia <sgkartist@gmail.com>
2021-04-26 13:42:55 -0600

    Angular frontend moved into MangiAmici's src/main/resources/static

--- ngMangiamici/src/app/app.module.ts
+++ ngMangiamici/src/app/app.module.ts
@@ -12,12 +12,14 @@ import { FooterComponent } from './components/footer/footer.component';
 import { PublicLandingComponent } from './components/public-landing/public-landing.component';
 import { PrivateLandingComponent } from './components/private-landing/private-landing.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { RestaurantComponent } from './components/restaurant/restaurant.component';
+
 import { RegisterComponent } from './components/register/register.component';
 import { RestaurantDashboardComponent } from './components/restaurant-dashboard/restaurant-dashboard.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderSearchComponent } from './components/header-search/header-search.component';
+
 import { FormsModule } from '@angular/forms';
+import { HeaderSearchComponent } from './components/header-search/header-search.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
