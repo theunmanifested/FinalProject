2b88efb9fcca3f85ea40935efaace71f39d86603
Joshua Rose <jlr007@protonmail.com>
2021-04-26 19:54:54 -0400

    User status functionality complete, nav bar MyProfile disappears if not logged in

--- ngMangiamici/src/app/components/user-profile/user-profile.component.ts
+++ ngMangiamici/src/app/components/user-profile/user-profile.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { UserStatus } from 'src/app/models/user-status';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { RestaurantService } from 'src/app/services/restaurant.service';
