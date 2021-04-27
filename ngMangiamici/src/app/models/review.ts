1b7225fa49d3baa78a1994bdb040639be6aa82ca
Sofia <sgkartist@gmail.com>
2021-04-27 15:29:54 -0600

    User can upload images for their reviews. - the images are displayed.

--- ngMangiamici/src/app/models/review.ts
+++ ngMangiamici/src/app/models/review.ts
@@ -24,7 +24,6 @@ export class Review {
     this.enabled = enabled;
     this.isPublic = isPublic;
     this.rating = rating;
-
     this.createdDate = createDate;
     this.updateDate = updateDate;
     this.enabled = enabled;
